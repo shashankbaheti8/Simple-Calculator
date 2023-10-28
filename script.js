7dd3709d7bb48fb8c0efb7528b75c5ec030bc028
shashankbaheti8 <shashankbaheti8@gmail.com>
2023-10-29 00:04:04 +0530

    first-commit

---
+++ script.js
@@ -0,0 +1,43 @@
+let string = "";
+let buttons = document.querySelectorAll('.button');
+
+Array.from(buttons).forEach((button)=>{
+  button.addEventListener('click', (e)=>{
+    //evaluating result
+    if(e.target.innerHTML == '='){
+      string = eval(string);
+      document.querySelector('input').value = string;
+    }
+
+    //clear
+    else if(e.target.innerHTML == 'C'){
+      string = ""
+      document.querySelector('input').value = string;
+    }
+    
+    //delete
+    else if (e.target.innerHTML == 'Del') {
+        if(string.length > 1){
+            string = string.slice(0, -1);
+            inputField.value = string;
+        }
+        
+        if(string.length = 1){
+            string = "";
+            document.querySelector('input').value = string;
+        }
+    }
+
+    //background-color change
+    else if (e.target.innerHTML == 'Magic') {
+        const color = prompt("Which backgorund color due you want?");
+        document.body.style.backgroundColor = color;
+    }
+    //creating expression
+    else{ 
+    console.log(e.target)
+    string = string + e.target.innerHTML;
+    document.querySelector('input').value = string;
+      }
+  })
+})
